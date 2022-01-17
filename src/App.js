c6f710c3d2aa262ef339a00278c7726966de7f66
Ankur Seth <iankurseth@gmail.com>
2022-01-17 15:05:25 +0530

    c0 with Hallo Welt * create-react-app * downgraded `mini-css-extract-plugin` to `2.4.6` * initiated `tailwind` * added `homepage` and `deploy` command to `package.json` * cleaned up default styles and markup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h2 className="bg-fuchsia-600 text-white text-2xl p-12">Hallo Welt</h2>
+			</header>
+		</div>
+	);
+}
+
+export default App;
